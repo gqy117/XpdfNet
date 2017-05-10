49a60fcc88553c4b9d1c16cd20dc6b81692647fe
Leo Li <Leo.Li@myob.com>
2017-05-11 11:56:52 +1200

    [develop] Removed all line breaks.

--- XpdfNet.Test/XpdfHelperTest.cs
+++ XpdfNet.Test/XpdfHelperTest.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Xunit;
+using System.Text.RegularExpressions;
 
 namespace XpdfNet.Test
 {
@@ -18,9 +19,12 @@ namespace XpdfNet.Test
             // Act
             string actual = xpdfHelper.ToText("./sample1.pdf");
             actual = actual.Replace("\f", "");
+            actual = Regex.Replace(actual, @"\r\n?|\n", string.Empty);
 
             // Assert
             string expected = File.ReadAllText(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Expected.txt"));
+            expected = Regex.Replace(expected, @"\r\n?|\n", string.Empty);
+
             Assert.Equal(expected, actual);
         }
     }
