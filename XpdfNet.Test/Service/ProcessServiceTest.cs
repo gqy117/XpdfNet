ce941246b652ff629ea9497d297ca9ddec521e32
Leo Li <gqy117@gmail.com>
2017-05-11 21:22:25 +1200

    [develop] Added user friendly error message.

---
+++ XpdfNet.Test/Service/ProcessServiceTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace XpdfNet.Test
+{
+    public class ProcessServiceTest
+    {
+        private ProcessService ProcessService;
+
+        [Fact]
+        public void ToText_ShouldThrowExceptionWithPathInformation_WhenPDFToTextExeCanNotBeFound()
+        {
+            // Arrange
+            string filename = "nonExistingfile.exe";
+            string arguments = "arg1 arg2";
+            string workingDirectory = "./";
+
+            this.ProcessService = new ProcessService(filename, arguments, workingDirectory);
+
+            // Act
+            Exception ex = Assert.Throws<Exception>(() => this.ProcessService.StartAndWaitForExit());
+
+            // Assert
+            string expected = $"The system cannot find the file specified. Filename: {filename}, WorkingDirectory: {workingDirectory}.";
+            Assert.Equal(expected, ex.Message);
+        }
+    }
+}
